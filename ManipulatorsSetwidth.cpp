1e66dcbfb7c5f3140cff97763979bd553b2f506d
Pilaniya1 <82079006+Pilaniya1@users.noreply.github.com>
2021-07-06 12:40:03 +0530

    Add files via upload
    how to use manipulators.
    the two most common manipulators in c++ are endl and setw(set width).

---
+++ ManipulatorsSetwidth.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int main()
+{
+int Basic = 950, Allowance = 95,Total = 1045;
+ 
+ cout<< setw(10) << "Basic "  <<Basic <<endl;
+ cout<< setw(10) << "Allowence " <<Allowance <<endl;
+ cout<< setw(10) << "Total" <<Total <<endl;
+     return 0;
+}
+//Character strings are also printed right-justified.
